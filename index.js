3dc11598d175ecb9fdf597c0949a40ee3fe58567
Ross Perkel <rossperkel@gmail.com>
2019-09-02 13:04:53 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" }
+]
+
+function superbowlWin(array){
+  const recordObj = (array.find( listObj => { return listObj.result === 'W'}))
+
+  if (recordObj){
+    return recordObj.year
+  }
+
+  return undefined
+}
+
+superbowlWin(record)
+
